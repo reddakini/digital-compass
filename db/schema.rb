26aa659437af5ec766d2d8f82d1fcdec37a0cdf6
Gabriela Felder <gab.felder@bluewin.ch>
2022-11-21 16:30:52 +0800

    Create Model Pathway

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_071734) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_082756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pathways", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
