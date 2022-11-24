be3cc26667f6c6cb4a229dd8a8faee82eece8ee2
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-24 16:08:47 +0800

    charts, tabs, nooooot done

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
-  
-  def dashboard
 
+  def dashboard
+    @progress_chart = {Quiz: 7, Skills: 2}
+    @result_chart = Pathway.group(:name).count
   end
 end
