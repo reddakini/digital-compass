afaec4df18497e410ec1d25c918b9e57b62879a3
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-24 18:07:40 +0800

    got the embed to work, but only quick and dirty, needs some fixing

--- app/javascript/controllers/typeform_controller.js
+++ app/javascript/controllers/typeform_controller.js
@@ -1,11 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 import { createWidget } from '@typeform/embed'
-import '@typeform/embed/build/css/widget.css'
-createWidget('<form-id>', { container: document.querySelector('#form') })
 
 // Connects to data-controller="typeform"
 export default class extends Controller {
   connect() {
-    console.log("Hello Typeform");
+    // console.log("Hello Typeform");
+    createWidget('wuDccAgU', { container: document.querySelector('#form') })
   }
 }
