85de41f2863428343fd4f0c2794380c1fbefc9d8
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-24 23:22:38 +0800

    tabs styling

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
 
   def dashboard
-    @progress_chart = {Quiz: 7, Skills: 2}
+    @progress_chart = {Done: 7, Left: 2}
     @result_chart = Pathway.group(:name).count
   end
 end
