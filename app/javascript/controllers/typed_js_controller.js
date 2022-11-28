a93a905a378a72b0e9120388a1670f17ecdb5bc8
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-28 13:07:19 +0800

    change navbar

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,16 +4,16 @@ import Typed from "typed.js"
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
-    console.log("hi from the controller");
-      var typed3 = new Typed(this.element, {
-        strings: [
-          'Finding a path into Tech...',
-          'Find YOUR path into Tech !'
-        ],
-        typeSpeed: 40,
-        backSpeed: 40,
-        smartBackspace: true, // this is a default
-        loop: true
+    // console.log("hi from the controller");
+    var typed3 = new Typed(this.element, {
+      strings: [
+        'Finding a path into Tech...',
+        'Find YOUR path into Tech !'
+      ],
+      typeSpeed: 40,
+      backSpeed: 40,
+      smartBackspace: true, // this is a default
+      loop: true
     })
   }
 }
