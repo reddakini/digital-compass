4e6be18a2a5ad8981e9bd7da171a51e68d22f56e
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-28 16:43:41 +0800

    skills first part/new

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -2,4 +2,26 @@ class UserSkillsController < ApplicationController
   def new
     @user_skill = UserSkill.new
   end
+
+  def create
+    selected_skills = params[:user_skill][:skill_id]
+
+    # clear the user skills
+    #selected_skills.destroy_all
+    selected_skills.each do |skill|
+      @user_skill = UserSkill.create(skill_id: skill, user_id: current_user.id) if skill != ""
+    end
+    redirect_to dashboard_path
+  end
+
+  def show
+    @user_skill = UserSkill.find(params[:id])
+  end
+
+  private
+
+  def user_skill_params
+    params.require(:user_skill).permit(skill_id: [])
+  end
+
 end
