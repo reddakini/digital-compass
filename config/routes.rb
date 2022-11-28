583a058826fbe4a424517d7b8d0fb8f8ad2bb1e6
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-28 13:09:00 +0800

    skills-new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :pathways, only: [:index, :show]
+  resources :user_skills, only: [:new, :create]
 
   get "/dashboard", to: "dashboards#dashboard"
   get "/assessment", to: "pages#assessment"
