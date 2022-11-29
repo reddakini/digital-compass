9b60ae5069ffc252d4044bb8035f3cf33ae31d7d
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-29 18:49:52 +0800

    started matchScore display in dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,8 @@ class DashboardsController < ApplicationController
     @result_chart = Pathway.group(:name).count
     @user_value = UserValue.new
     @user_skills = current_user.skills
+    @recommendation = Recommendation.where(user: current_user).last
+    @recommended_pathways = RecommendedPathway.where(recommendation: @recommendation)
+    # @recommended_pathways.sort_by! { |pathway| pathway.match_score.value }
   end
 end
