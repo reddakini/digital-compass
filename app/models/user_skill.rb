120d76730cdebd80cadd42d11e3db8471c3b1080
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-29 17:48:21 +0800

    button not fixed but the lofic for skills is implemented

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -2,5 +2,7 @@ class UserSkill < ApplicationRecord
   belongs_to :skill
   belongs_to :user
 
-  
+  validates :user, uniqueness: {scope: :skill}
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :user_skills, only: [:new, :create]
   resources :user_values, only: [:new, :create]
 
+  patch "/user_skills_level", to: "user_skills#update_level", as: :update_level
   get "/dashboard", to: "dashboards#dashboard"
   get "/assessment", to: "pages#assessment"
   get "/recommendations/add", to: "recommendations#add"
